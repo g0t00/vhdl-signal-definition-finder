e68ceccb0d9bb36776ee518cf23c54cd135b8939
Anton Schulte <mail@anton-schulte.de>
2018-06-29 18:31:59 +0200

    First Commit

---
+++ spec/vhdl-signal-definition-finder-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import VhdlSignalDefinitionFinderView from '../lib/vhdl-signal-definition-finder-view';
+
+describe('VhdlSignalDefinitionFinderView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
